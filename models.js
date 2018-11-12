cefa0ec335c734de4e9be7775efd1125a636ab07
MCClarke <mcarlotclarke@gmail.com>
2018-11-12 18:05:46 -0500

    Plantr next steps

--- models.js
+++ models.js
@@ -1,6 +1,23 @@
 const Sequelize = require('sequelize');
-const pg = require('pg');
-const pgHstore = require('pg-hstore');
 const db = new Sequelize(`postgres://localhost:5432/plantr`);
 
 module.exports = db;
+
+const Gardener = db.define('garderner', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+});
+
+const Plot = db.define('plot', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+});
+
+const Vegetable = db.define('vegetable', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  plantedOn: Sequelize.DATE // originally planted_on not in camel case
+});
+
+Vegetable.belongsToMany(Plot, {through: 'veggiePlot'});
+Plot.belongsToMany(Vegetable, {through: 'veggiePlot'});
